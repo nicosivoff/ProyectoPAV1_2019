c42c5f58909cd2dd7cf15db9ca788b1c37d92a02
Lucas Martin <lucasmartin766@gmail.com>
2019-09-29 17:40:45 -0300

    ABM Marca

---
+++ TP1-Lucas/WindowsFormsApplication1/BusinessLayer/MarcaService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrabajoPractico.DataAccessLayer;
+
+namespace TrabajoPractico.BusinessLayer
+{
+    class MarcaService
+    {
+        private MarcaDao oMarcaDao;
+        public MarcaService()
+        {
+            oMarcaDao = new MarcaDao();
+        }
+        public IList<Marca> obtenerTodos()
+        {
+            return oMarcaDao.getAll();
+        }
+        public IList<Marca> obtenerConFiltros(String condiciones)
+        {
+            return oMarcaDao.getMarcaCondicionada(condiciones);
+        }
+        public object obtenerMarca(string idMarca)
+        {
+            return oMarcaDao.getMarcaSinParametros(idMarca);
+        }
+        public bool crearMarca(Marca marca)
+        {
+            return oMarcaDao.crearMarca(marca);
+        }
+        public bool actualizarMarca(Marca marca)
+        {
+            return oMarcaDao.actualizarMarca(marca);
+        }
+        public bool modificarEstadoMarca(Marca marca)
+        {
+            return oMarcaDao.deleteMarca(marca);
+        }
+        
+    }
+}
--- TP1-Lucas/WindowsFormsApplication1/Program.cs
+++ TP1-Lucas/WindowsFormsApplication1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TrabajoPractico.GUILayer;
 
 namespace TrabajoPractico
 {
@@ -16,7 +17,7 @@ namespace TrabajoPractico
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmLogin());
+            Application.Run(new frmMarca());
             
         }
     }
